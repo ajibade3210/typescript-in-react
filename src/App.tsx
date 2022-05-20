67baa7705eb7229188581376ae484e3bd68d8747
ajibade3210 <ajibade69th@gmail.com>
2022-05-20 18:22:44 +0100

    Types for State And Props In React

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-//type declaration
-interface MessageProps {
+interface IProps {
+  staff: string;
   message: string;
+}
+
+interface IState {
+  position: string;
+  experience: number;
 }
 
 function App() {
   return (
     <>
-      <Employee message="To Typescript In React" />
+      <Employee message="Good Morning" staff="Ryan" />
     </>
   );
 }
 
-// change "Employee" component
-const Employee: React.FC<MessageProps> = ({ message }) => {
+function Employee({ staff, message }: IProps) {
+  let [state, setState] = useState<IState>({
+    position: "Software Engineer",
+    experience: 17,
+  });
   return (
-    <>
-      <h1>Welcome {message}</h1>
-    </>
+    <div className="App">
+      <h2>
+        {message} {staff}
+      </h2>
+      <ul>
+        <li>
+          <span>Position:</span> {state.position}
+        </li>
+        <li>
+          <span>Year Of Experience:</span> {state.experience}
+        </li>
+      </ul>
+    </div>
   );
-};
+}
 
 export default App;
