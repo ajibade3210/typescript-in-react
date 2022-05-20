c8c9429ea8a8e7e9d6d305d343e51bda7c03b07f
ajibade3210 <ajibade69th@gmail.com>
2022-05-20 18:26:32 +0100

    Event And Input Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,70 @@
 import React, { useState } from "react";
 import "./App.css";
 
-interface IProps {
-  staff: string;
-  message: string;
+interface IProps {}
+
+interface IUser {
+  username: string;
+  password: string;
 }
 
 interface IState {
-  position: string;
-  experience: number;
+  user: IUser;
 }
 
 function App() {
   return (
     <>
-      <Employee message="Good Morning" staff="Ryan" />
+      <LoginForm />
     </>
   );
 }
 
-function Employee({ staff, message }: IProps) {
-  let [state, setState] = useState<IState>({
-    position: "Software Engineer",
-    experience: 17,
+function LoginForm() {
+  const [state, setState] = useState<IState>({
+    user: { username: "", password: "" },
   });
+
+  let updateInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setState({
+      user: {
+        ...state.user,
+        [event.target.name]: event.target.value,
+      },
+    });
+  };
+
+  let login = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(state.user);
+  };
+
   return (
     <div className="App">
-      <h2>
-        {message} {staff}
-      </h2>
-      <ul>
-        <li>
-          <span>Position:</span> {state.position}
-        </li>
-        <li>
-          <span>Year Of Experience:</span> {state.experience}
-        </li>
-      </ul>
+      <h2>Login Employee</h2>
+      <form onSubmit={login}>
+        <div>
+          <input
+            name="username"
+            value={state.user.username}
+            onChange={updateInput}
+            type="text"
+            placeholder="Username"
+          />
+        </div>
+        <div>
+          <input
+            name="password"
+            value={state.user.password}
+            onChange={updateInput}
+            type="password"
+            placeholder="Password"
+          />
+        </div>
+        <div>
+          <input type="submit" />
+        </div>
+      </form>
     </div>
   );
 }
