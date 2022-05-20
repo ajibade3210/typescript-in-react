fcb59348f872e2651ac54cf3300586c945f77499
ajibade3210 <ajibade69th@gmail.com>
2022-05-20 18:35:17 +0100

    Generating types for JSON Documents

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,93 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
-interface IProps {}
-
-interface IUser {
-  username: string;
-  password: string;
+// Generated interface type from "quicktype"
+interface User {
+  userId: number;
+  id: number;
+  title: string;
 }
 
 interface IState {
-  user: IUser;
+  loading: boolean;
+  users: User[];
+  errorMessage: string;
 }
 
-function App() {
+function App(): JSX.Element {
   return (
     <>
-      <LoginForm />
+      <JobLog />
     </>
   );
 }
 
-function LoginForm() {
+function JobLog() {
   const [state, setState] = useState<IState>({
-    user: { username: "", password: "" },
+    loading: false,
+    users: [] as User[],
+    errorMessage: "",
   });
 
-  let updateInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setState({
-      user: {
-        ...state.user,
-        [event.target.name]: event.target.value,
-      },
-    });
-  };
-
-  let login = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(state.user);
-  };
+  useEffect(() => {
+    setState({ ...state, loading: true });
+    UserService.getAllUsers()
+      .then((res) => {
+        setState({
+          ...state,
+          loading: false,
+          users: res.data,
+        });
+      })
+      .catch((err) => {
+        setState({
+          ...state,
+          loading: false,
+          errorMessage: err.message,
+        });
+      });
+  }, []);
 
   return (
     <div className="App">
-      <h2>Login Employee</h2>
-      <form onSubmit={login}>
-        <div>
-          <input
-            name="username"
-            value={state.user.username}
-            onChange={updateInput}
-            type="text"
-            placeholder="Username"
-          />
-        </div>
-        <div>
-          <input
-            name="password"
-            value={state.user.password}
-            onChange={updateInput}
-            type="password"
-            placeholder="Password"
-          />
-        </div>
-        <div>
-          <input type="submit" />
-        </div>
-      </form>
+      <h1>COB Summary</h1>
+      <div className="">
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Created By User</th>
+              <th>Job Summary</th>
+            </tr>
+          </thead>
+          <tbody className="">
+            {state.users.length > 0 &&
+              state.users.map((user) => {
+                return (
+                  <tr key={user.id} style={{ border: "2px solid black" }}>
+                    <td>{user.id}</td>
+                    <td>{user.userId}</td>
+                    <td>{user.title}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+          <caption>Employee Job Log Summary</caption>
+        </table>
+      </div>
     </div>
   );
+}
+
+//Fetching API Using Axios
+export class UserService {
+  private static serverURL: string = `https://jsonplaceholder.typicode.com`;
+
+  public static getAllUsers() {
+    let dataURL: string = `${this.serverURL}/albums`;
+    return axios.get(dataURL);
+  }
 }
 
 export default App;
