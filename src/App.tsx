15ef4293679ed5489dbdaf070a4bb0417e90e79c
ajibade3210 <ajibade69th@gmail.com>
2022-05-21 02:08:58 +0100

    Create Promise Using Typescript In React

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <h1>Hello World</h1>;
-}
+  useEffect(() => {
+    const pickEvenNumber = new Promise<number>((resolve, reject) => {
+      const isEvenNumber = Math.floor(Math.random() * 20);
+      if (isEvenNumber % 2 === 0) {
+        resolve(isEvenNumber);
+        return;
+      }
+      reject(isEvenNumber);
+    });
+    pickEvenNumber
+      .then((res) => console.log("Resolved: Even Number " + res))
+      .catch((err) => console.log("Rejected: Not Even Number", err));
+  }, []);
 
-//add "Employee" component .....
+  return (
+    <div className="App">
+      <h1>Make A Promises with Typescript. ❤</h1>
+    </div>
+  );
+}
 
 export default App;
